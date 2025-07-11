a9868dd03d8dde8641d2e9b0c9aaacdbe5d8c898
Oscar CENTENO MORA <OskyRosky@users.noreply.github.com>
2025-07-10 18:22:52 -0600

    Opción slow_mo
    browser = await p.chromium.launch(headless=False, slow_mo=500)

--- Mod3/backend/app/DESCARGA_SIGED.py
+++ Mod3/backend/app/DESCARGA_SIGED.py
@@ -31,14 +31,8 @@ async def descargar_documentos(url, notificar=None):
     os.makedirs(ruta_descarga, exist_ok=True)
 
     async with async_playwright() as p:
-        browser = await p.chromium.launch(
-            headless=True,
-            args=["--start-maximized"]
-        )
-        context = await browser.new_context(
-            accept_downloads=True,
-            viewport={"width": 1920, "height": 1080}
-        )
+        browser = await p.chromium.launch(headless=False, slow_mo=500)
+        context = await browser.new_context(accept_downloads=True)
         page = await context.new_page()
 
         msg = "🔄 Cargando la página principal..."
